f8a98bbd156483d414217714b0d537d54d297da3
Yonas Sh <yonakifle@yahoo.com>
2023-06-11 03:08:50 -0700

    added javascript

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const list = document.querySelector('.menu');
+const lists = document.querySelector('.menu ul');
+const items = document.querySelector('.menu li');
+const menu = document.querySelector('.menu');
+const contents = document.querySelectorAll('.content')
 
-console.log(list.child)
+
+const removeStyle = (e)=>{
+   Array.from(lists.children)
+    .forEach(list =>
+        list.classList.remove('active')
+        )
+ 
+   e.target.classList.add('active') 
+}
+
+
+const displayContent = (e)=>{
+    contents.forEach(content => content.classList.remove('active'))
+
+    const target = e.target.getAttribute('data-target');
+   document.querySelector(target).classList.add('active');
+}
+
+lists.addEventListener('click',e =>{
+    removeStyle(e);
+    displayContent(e)
+})
